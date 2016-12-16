5df34e49efff940ce42b88b3c2234a3fdd944d19
CorsoB-Informatica <corso.b.informatica.2016@gmail.com>
2016-12-16 10:40:20 +0100

    First commit

---
+++ Punto/src/it/giordanistriano/introduzioneJSE_1_8/Punto.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package it.giordanistriano.introduzioneJSE_1_8;
+
+/**
+ * @author Michele
+ *
+ */
+public class Punto {
+	public int x;    // coordinata dell'asse delle ascisse
+	public int y;    //coordinata dell'asse delle ordinate
+}
---
+++ Punto/src/it/giordanistriano/introduzioneJSE_1_8/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Michele
+ *
+ */
+package it.giordanistriano.introduzioneJSE_1_8;
